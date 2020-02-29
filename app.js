6c30967653f4561d369a8d4f3d5a6033a295eedd
Lone Nilsson <ln222tu@student.lnu.se>
2020-02-29 20:30:08 +0100

    commit

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-console.log('test test')
+document.write('welcome to my app')
+console.log('app loaded')
+
 app.get('/', (req, res) => res.send('Hello World! This is a deployed app now, or is it?'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
